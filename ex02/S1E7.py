38ba72da739bec76d87a24d6bb555bd5da696d9f
soniavarela <svarelavila@gmail.com>
2024-08-24 18:13:09 +0200

    ex OOP

---
+++ ex02/S1E7.py
@@ -0,0 +1,90 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """
+        Initialize a Baratheon character with the
+        provided first name.
+
+        Parameters:
+            first_name (str): The first name of the character.
+            is_alive (bool): The alive status of the character.
+            Defaults to True.
+        """
+        super().__init__(first_name, is_alive)
+        self.family_name = 'Baratheon'
+        self.eyes = 'brown'
+        self.hairs = 'dark'
+
+    def die(self):
+        """Mark the character as not alive."""
+        self.is_alive = False
+
+    def __str__(self):
+        """
+        Return a string representation of
+        the Baratheon character.
+        """
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """
+        Return a formal string representation of
+        the Baratheon character.
+        """
+        return self.__str__()
+
+
+class Lannister(Character):
+    """Representing the Lannister family."""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """
+        Initialize a Lannister character with the provided first name.
+
+        Parameters:
+            first_name (str): The first name of the character.
+            is_alive (bool): The alive status of the character.
+            Defaults to True.
+        """
+        super().__init__(first_name, is_alive)
+        self.family_name = 'Lannister'
+        self.eyes = 'blue'
+        self.hairs = 'light'
+
+    def die(self):
+        """Mark the character as not alive."""
+        self.is_alive = False
+
+    def __str__(self):
+        """
+        Return a string representation of
+        the Lannister character.
+        """
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """
+        Return a formal string representation of
+        the Lannister character.
+        """
+        return self.__str__()
+
+
+@classmethod
+def create_lannister(cls, first_name, is_alive=True):
+    """
+    Factory method to create a Lannister character.
+
+    Parameters:
+        first_name (str): The first name of the character.
+        is_alive (bool): The alive status of the character.
+        Defaults to True.
+
+    Returns:
+        Lannister: An instance of the Lannister class.
+    """
+    return cls(first_name, is_alive)
---
+++ ex04/ft_calculator.py
@@ -0,0 +1,38 @@
+class Calculator:
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """
+        Calculate the dot product of two vectors and print the result.
+
+        Parameters:
+        V1 (list of float): The first vector.
+        V2 (list of float): The second vector.
+        """
+        dot_product = sum(x * y for x, y in zip(V1, V2))
+        print(f"Dot product is: {dot_product:.1f}")
+
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """
+        Add two vectors element-wise and print the resulting vector.
+
+        Parameters:
+        V1 (list of float): The first vector.
+        V2 (list of float): The second vector.
+        """
+        result = [x + y for x, y in zip(V1, V2)]
+        print(f"Add Vector is : {[f'{val:.1f}' for val in result]}")
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """
+        Subtract the second vector from the first vector element-wise 
+        and print the resulting vector.
+
+        Parameters:
+        V1 (list of float): The first vector.
+        V2 (list of float): The second vector.
+        """
+        result = [x - y for x, y in zip(V1, V2)]
+        print(f"Sous Vector is: {[f'{val:.1f}' for val in result]}")
+
