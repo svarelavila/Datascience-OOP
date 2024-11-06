18b1c6974f5d8e0070b17c979ea878bfb6b424c0
soniavarela <svarelavila@gmail.com>
2024-11-06 18:23:41 +0100

    retoques

--- ex02/DiamondTrap.py
+++ ex02/DiamondTrap.py
@@ -1,26 +1,30 @@
 from S1E7 import Baratheon, Lannister
 
-
 class King(Baratheon, Lannister):
-    """This class represents King Joffrey Baratheon,
-    inheriting from both Baratheon and Lannister."""
+    """The mad King himself"""
+    
     def __init__(self, first_name, is_alive=True):
-        """Initialize the King with
-        the characteristics from both families."""
+        """Initializes the king, a cross between a Baratheon and Lannister"""
         super().__init__(first_name, is_alive)
+        self._eyes = "brown"  # Valor predeterminado para los ojos
+        self._hairs = "dark"  # Valor predeterminado para el cabello
 
-    def set_eyes(self, color):
-        """Set the eye color of the King."""
-        self.eyes = color
+    def get_eyes(self) -> str:
+        """Getter for eye color"""
+        return self._eyes
 
-    def set_hairs(self, color):
-        """Set the haur color of the King."""
-        self.hairs = color
+    def set_eyes(self, color: str) -> None:
+        """Setter for eye color"""
+        self._eyes = color
 
-    def get_eyes(self):
-        """Get the eye color of the King."""
-        return self.eyes
+    def get_hairs(self) -> str:
+        """Getter for hair color"""
+        return self._hairs
 
-    def get_hairs(self):
-        """Get the hair color of the King."""
-        return self.hairs
+    def set_hairs(self, color: str) -> None:
+        """Setter for hair color"""
+        self._hairs = color
+
+    # Definición de propiedades con documentación corregida
+    eyes = property(get_eyes, set_eyes, doc="Property for eye color")
+    hairs = property(get_hairs, set_hairs, doc="Property for hair color")
